774ab5b7c75db7f3936a1cf8cf67e71961a025e8
Matt McKenzie <mattmckenzie@hotmail.com>
2017-04-04 16:56:45 +1000

    photo stick project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,8 @@ export class AppComponent {
 
   title = 'Axe and Sirens';
 
+  productLink = "/public/images/routes/products-button-blue.png"
+
   initModal: boolean;
   showDialog: boolean;
   currentProductInModal: Product;
